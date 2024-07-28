474796f450593af1130eed4adff4843089cea6db
nikita-skobov <nskobov.websites@gmail.com>
2024-07-28 15:46:06 -0600

    changes example to read and write from wasm memory and to pass string data

--- examplelib/src/lib.rs
+++ examplelib/src/lib.rs
@@ -1,4 +1,36 @@
+#[link(wasm_import_module = "host")]
+extern "C" {
+  fn get_host_data_size() -> u32;
+  fn get_host_data(ptr: *const u8, len: u32);
+  fn set_host_data(ptr: *const u8, len: u32);
+}
+
+unsafe fn fill_data_from_host() -> Vec<u8> {
+  let data = unsafe {
+    let len = get_host_data_size() as usize;
+    let mut data: Vec<u8> = Vec::with_capacity(len);
+    data.set_len(len);
+    let ptr = data.as_ptr();
+    let len = data.len();
+    get_host_data(ptr, len as _);
+    data
+  };
+  data
+}
+
+unsafe fn set_data_for_host(out_data: Vec<u8>) {
+  unsafe {
+    let ptr = out_data.as_ptr();
+    let len = out_data.len();
+    set_host_data(ptr, len as _);
+  }
+}
+
+// return how many spaces are in the string the host passed to us
 #[no_mangle]
-extern "C" fn gcd(a: i32, b: i32) -> i32 {
-  a + b
+extern "C" fn string_example() -> u32 {
+  let data = unsafe { fill_data_from_host() };
+  let s = String::from_utf8_lossy(&data).to_string();
+  unsafe { set_data_for_host("abcd!".as_bytes().to_vec()) };
+  s.chars().filter(|c| *c == ' ').count() as u32
 }
--- wasmrunner/src/main.rs
+++ wasmrunner/src/main.rs
@@ -1,62 +1,71 @@
 use wasmtime::*;
 
-const WASM_CONTENTS: &'static str = r#"
-(module
-    (func $gcd (param i32 i32) (result i32)
-      (local i32)
-      block  ;; label = @1
-        block  ;; label = @2
-          local.get 0
-          br_if 0 (;@2;)
-          local.get 1
-          local.set 2
-          br 1 (;@1;)
-        end
-        loop  ;; label = @2
-          local.get 1
-          local.get 0
-          local.tee 2
-          i32.rem_u
-          local.set 0
-          local.get 2
-          local.set 1
-          local.get 0
-          br_if 0 (;@2;)
-        end
-      end
-      local.get 2
-    )
-    (export "gcd" (func $gcd))
-  )
-"#;
-
 /// use WASM_CONTENTS string by default, or if file path provided
 /// load wasm from file
 fn main() {
-    let use_wasm_binary_path = std::env::args().nth(1);
-    let mut store = Store::<()>::default();
-    let module = if let Some(path) = use_wasm_binary_path {
-        if let Ok(contents) = std::fs::read(path) {
-            Module::new(store.engine(), contents)
-        } else {
-            Module::new(store.engine(), WASM_CONTENTS)
+    let use_wasm_binary_path = std::env::args().nth(1).expect("Must provide path to wasm file");
+    let engine = Engine::default();
+    let wasm_contents = std::fs::read(use_wasm_binary_path).expect("Failed to read wasm file");
+
+    let mut store: Store<_> = Store::new(&engine, vec![b'h', b'e', b'l', b'l', b'o', b'w', b' ', b'w', b'o', b'r', b'l', b'd']);
+    let module = Module::new(store.engine(), wasm_contents).expect("Failed to compile wasm module");
+    let mut linker: Linker<_> = Linker::new(store.engine());
+    linker
+        .func_wrap(
+            "host",
+            "get_host_data_size",
+            |caller: Caller<'_, _>| -> u32 {
+                let data: &Vec<u8> = caller.data();
+                data.len() as u32
+            },
+        )
+        .unwrap();
+
+    linker.func_wrap("host", "get_host_data", |mut caller: Caller<'_, _>, ptr: u32, len: u32| {
+        let ptr = ptr as usize;
+        let len = len as usize;
+        let host_data: Vec<u8> = std::mem::take(caller.data_mut());
+        if host_data.len() != len {
+            return;
         }
-    } else {
-        Module::new(store.engine(), WASM_CONTENTS)
-    }
-    .expect("Failed to compile wasm module");
+        if let Some(Extern::Memory(mem)) = caller.get_export("memory") {
+            let mem_data = mem.data_mut(&mut caller);
+            if let Some(data) = mem_data.get_mut(ptr..ptr+len) {
+                data.copy_from_slice(&host_data);
+            }
+        }
+        *caller.data_mut() = host_data;
+    }).unwrap();
+    linker.func_wrap("host", "set_host_data", |mut caller: Caller<'_, _>, ptr: u32, len: u32| {
+        let ptr = ptr as usize;
+        let len = len as usize;
+        let output = if let Some(Extern::Memory(mem)) = caller.get_export("memory") {
+            let mem_data = mem.data_mut(&mut caller);
+            if let Some(data) = mem_data.get_mut(ptr..ptr+len) {
+                Some(data.to_vec())
+            } else {
+                None
+            }
+        } else { None };
+        if let Some(out) = output {
+            let host_data: &mut Vec<u8> = caller.data_mut();
+            *host_data = out;
+        }
+    }).unwrap();
 
+    let instance = linker
+        .instantiate(&mut store, &module)
+        .expect("Failed to instantiate wasm module");
 
-    let instance =
-        Instance::new(&mut store, &module, &[]).expect("Failed to instantiate wasm module");
+    let entrypoint_func = match instance.get_typed_func::<(), u32>(&mut store, "string_example") {
+        Ok(tf) => tf,
+        Err(_) => {
+            println!("Unable to find wasm_entrypoint");
+            return;
+        }
+    };
 
-    // Invoke `gcd` export
-    let gcd = instance
-        .get_typed_func::<(i32, i32), i32>(&mut store, "gcd")
-        .expect("Failed to find gcd function in wasm module");
-    println!(
-        "gcd(6, 27) = {}",
-        gcd.call(&mut store, (6, 27))
-            .expect("Failed to run wasm gcd function")
-    );
+    let resp = entrypoint_func.call(&mut store, ()).unwrap();
+    println!("RESP: {}", resp);
+    println!("MY DATA: {:?}", store.data());
 }
